77a8c02598c2f2457578dce16d96287e4642b753
ASPIRE5-PC\Administrator <0926alsl7@gmail.com>
2025-12-22 23:52:39 +0900

    12/22

--- dashboard.js
+++ dashboard.js
@@ -6,3 +6,26 @@ menuBtn.addEventListener("click", () => {
   aside.classList.toggle("open");
   wrap.classList.toggle("menu-open");
 });
+
+const navItems = document.querySelectorAll(".nav-item");
+const sections = document.querySelectorAll(".section");
+
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    const target = item.dataset.target;
+
+    navItems.forEach((i) => i.classList.remove("is-active"));
+    item.classList.add("is-active");
+
+    sections.forEach((section) => {
+      section.classList.toggle("is-active", section.dataset.page === target);
+    });
+  });
+});
+
+document.querySelectorAll(".progress-bar").forEach((bar) => {
+  const value = bar.dataset.value;
+  requestAnimationFrame(() => {
+    bar.style.width = value + "%";
+  });
+});
