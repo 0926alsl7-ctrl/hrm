6fe0607896ec85ecd17bea581cc4a486f8378175
ASPIRE5-PC\Administrator <0926alsl7@gmail.com>
2025-12-30 23:15:24 +0900

    1. schedule- month-mode-view 디자인 수정 2. vacation 동일하게 3. mode-view 버튼 섹션끼리 영향 미침 4. 모바일 구조 만들기

---
+++ data.js
@@ -0,0 +1,19 @@
+// (1) 직무
+const jobByDept = {
+  Management: "Management",
+  Sales: "Sales",
+  Marketing: "Marketing",
+  Design: "Design",
+  Production: "Production",
+  "R&D": "R&D",
+};
+
+// (2) 직원
+const employeesData = {
+  Management: ["정희석", "강대희", "이민", "권동주", "김준성", "권동현"],
+  Sales: ["이유정", "김민석", "김민지", "이유준", "이은빈", "김태환"],
+  Marketing: ["정하늘", "이담희", "정승훈", "김성길", "강대웅"],
+  Design: ["하다경", "이기자", "한진수", "박지원", "이은수", "권민지"],
+  Production: ["김형선", "이동욱", "이진", "김여원", "박채린"],
+  "R&D": ["김민이", "심진우", "진예진", "강민서", "최소윤", "장재영"],
+};
--- schedule.js
+++ schedule.js
@@ -38,6 +38,25 @@ const todayBtns = document.querySelectorAll(".today-btn");
 const mixToggle = document.getElementById("mixToggle");
 const scheduleHeaderMain = document.getElementById("scheduleHeaderMain");
 
+// // (1) 직무
+// const jobByDept = {
+//   Management: "Management",
+//   Sales: "Sales",
+//   Marketing: "Marketing",
+//   Design: "Design",
+//   Production: "Production",
+//   "R&D": "R&D",
+// };
+
+// // (2) 직원
+// const employeesData = {
+//   Management: ["정희석", "강대희", "이민", "권동주", "김준성", "권동현"],
+//   Sales: ["이유정", "김민석", "김민지", "이유준", "이은빈", "김태환"],
+//   Marketing: ["정하늘", "이담희", "정승훈", "김성길", "강대웅"],
+//   Design: ["하다경", "이기자", "한진수", "박지원", "이은수", "권민지"],
+//   Production: ["김형선", "이동욱", "이진", "김여원", "박채린"],
+//   "R&D": ["김민이", "심진우", "진예진", "강민서", "최소윤", "장재영"],
+// };
 /* ======================================================
    상태
 ====================================================== */
@@ -48,27 +67,6 @@ let editingId = null;
 
 const drafts = {};
 const saved = JSON.parse(localStorage.getItem("schedules") || "{}");
-
-/* ======================================================
-   데이터
-====================================================== */
-const jobByDept = {
-  Management: "Management",
-  Sales: "Sales",
-  Marketing: "Marketing",
-  Design: "Design",
-  Production: "Production",
-  "R&D": "R&D",
-};
-
-const employeesData = {
-  Management: ["정희석", "강대희", "이민", "권동주", "김준성", "권동현"],
-  Sales: ["이유정", "김민석", "김민지", "이유준", "이은빈", "김태환"],
-  Marketing: ["정하늘", "이담희", "정승훈", "김성길", "강대웅"],
-  Design: ["하다경", "이기자", "한진수", "박지원", "이은수", "권민지"],
-  Production: ["김형선", "이동욱", "이진", "김여원", "박채린"],
-  "R&D": ["김민이", "심진우", "진예진", "강민서", "최소윤", "장재영"],
-};
 
 /* ======================================================
    날짜
